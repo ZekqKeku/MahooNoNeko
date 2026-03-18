3235eda0c07d8184200aab7823d75cedcc97bf23
ZekqKeku <alekschojnowski@gmail.com>
2026-03-18 14:25:05 +0100

    Add video to downloader cog

--- cogs/downloader.py
+++ cogs/downloader.py
@@ -37,3 +37,30 @@ class Downloader(commands.Cog):
 
         await interaction.followup.send(content=content, ephemeral=True)
 
+    @nextcord.slash_command(
+        name="download_video",
+        description="Download a mp4 file",
+    )
+    async def download_video(self,
+        interaction: nextcord.Interaction,
+        url: str = nextcord.SlashOption(
+            name = "url",
+            description = "The URL to the youtube video, instagram reels, etc.",
+        )
+    ):
+        await interaction.response.defer(ephemeral=True)
+        file_name = baseUtils.Utils.random_name()
+
+        self.downloader.download_video(url, file_name)
+        path = self.downloader.get_path(file_name=file_name, ext="mp4")
+        file_id = self.pixeldrain.upload_file(path)
+        file_url = self.pixeldrain.get_download_link(file_id)
+        self.downloader.remove_file(path)
+
+        content = (
+            "Link do pobrania twojego pliku .mp4\n"
+            f"{file_url}"
+        )
+
+        await interaction.followup.send(content=content, ephemeral=True)
+
